b8325e91d42b5e3a28b88c04c9d353bc603c3869
Kangming Deng <dengkangming@gmail.com>
2026-01-24 20:32:53 -0500

    add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import htmlMinifier from "vite-plugin-html-minifier";
 
@@ -17,6 +18,11 @@ export default defineConfig({
       minify: true,
     }),
   ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   build: {
     rolldownOptions: {
       output: {
